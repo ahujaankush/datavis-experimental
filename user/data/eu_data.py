c96d22114fa424c0c1fa0f8c76c2aba529bf477d
Ankush Ahuja <aahuja06@proton.me>
2024-06-09 21:32:30 +0200

    :)

--- user/data/eu_data.py
+++ user/data/eu_data.py
@@ -15,34 +15,42 @@ csv_data = response.content.decode('utf-8')
 df = pd.read_csv(StringIO(csv_data))
 
 # Function to filter data based on country, unit, and waste operation
-def filter_data(country, unit, waste_operation):
-    filtered_df = df[(df['geo'] == country) & (df['unit'] == unit) & (df['wst_oper'] == waste_operation)]
+def filter_data(country, unit, waste_operation, category):
+    filtered_df = df[(df['geo'] == country) & (df['unit'] == unit) & (df['wst_oper'] == waste_operation) & (df['waste'] == category)]
     return filtered_df
 
 # Example: Filter data for each country, unit, and waste operation
 countries = df['geo'].unique()
 units = df['unit'].unique()
-waste_operations = df['wst_oper'].unique()
+operations = df['wst_oper'].unique()
+categories = df['waste'].unique()
 
 for country in countries:
     for unit in units:
-        for waste_operation in waste_operations:
-            filtered_data = filter_data(country, unit, waste_operation)
-            # Extract datapoints as arrays of objects
+        for operation in operations:    
+            datapoints = {}
             current_directory = os.path.dirname(os.path.realpath(__file__))
-            datapoints = list()
-            for index, row in filtered_data.iterrows():
-                store = {}
-                store['year'] = row['TIME_PERIOD']
-                store['value']= row['OBS_VALUE']
-                datapoints.append(store)
-
+            for categorie in categories:
+                filtered_data = filter_data(country, unit, operation, categorie)
+                # Extract datapoints as arrays of objects
+                for index, row in filtered_data.iterrows():
+                    key = row['TIME_PERIOD']
+                    if(not key in datapoints):
+                        datapoints[key] = 0
+                    datapoints[key] += row['OBS_VALUE']
+               
             if (len(datapoints) == 0): # Filter out tables with no data
                 continue
+    
+            data_arr = []
+            keys = datapoints.keys()
+
+            for key in keys:
+                data_arr.append({'year': key, 'value': datapoints[key]})
 
             # Export datapoints to JSON file
-            filename = f"{current_directory}/eu/{country}_{waste_operation}_{unit}.json"  # Example file name: "US_kg_COL.json"
+            filename = f"{current_directory}/eu/{country}_{operation}_{unit}.json"  # Example file name: "US_kg_COL.json"
             with open(filename, 'w') as file:
-                json.dump(datapoints, file)
+                json.dump(data_arr, file)
 
 print("Data exported successfully.")
